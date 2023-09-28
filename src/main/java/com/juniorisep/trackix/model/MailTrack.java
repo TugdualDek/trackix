49dd7dda930cb8f652564eacaa727322f005918f
t.de-kerdrel <t.de-kerdrel@bilendi.com>
2023-09-28 13:40:51 +0200

    fix (mailtrack) : fix problem

--- src/main/java/com/juniorisep/trackix/model/MailTrack.java
+++ src/main/java/com/juniorisep/trackix/model/MailTrack.java
@@ -1,9 +1,10 @@
 package com.juniorisep.trackix.model;
 
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.util.Date;
 import java.util.List;
 
@@ -18,21 +19,32 @@ public class MailTrack {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(nullable = false)
-    @OneToMany(mappedBy = "mailTrack", cascade = CascadeType.ALL)
-    private int campaignId;
+    private int campaignId; // Il s'agit d'une clé étrangère, vous pouvez utiliser @ManyToOne pour définir la relation.
+
     @Column(nullable = false)
     private String name;
+
     private String description;
+
     @Column(nullable = false)
     private Date startDate;
+
     @Column(nullable = false)
     private boolean isFinished;
+
     private String image;
+
     @OneToMany(mappedBy = "mailTrack", cascade = CascadeType.ALL)
-    @JsonManagedReference // Serialize this property normally
+    @JsonManagedReference
     private List<DataMail> dataMails;
+
     private String link;
+
     private int count;
 
+    @ManyToOne // Relation many-to-one avec Campaign
+    @JoinColumn(name = "campaignId", referencedColumnName = "id") // Assurez-vous que "campaignId" correspond au nom de la colonne dans la table
+    private Campaign campaign;
 }
